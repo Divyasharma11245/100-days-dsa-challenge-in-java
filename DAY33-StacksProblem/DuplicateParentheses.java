0d2deb29f241f9bc7926c36378724a9940a1996f
Divya Sharma <divyasharma11245@gmail.com>
2025-09-03 22:57:49 +0530

    DAY33- stack valid and duplicate parentheses

---
+++ DAY33-StacksProblem/DuplicateParentheses.java
@@ -0,0 +1,38 @@
+// package DAY33-StacksProblem;
+
+import java.util.*;
+
+public class DuplicateParentheses {
+
+    public static boolean isDuplicate(String str) {
+        Stack<Character> s = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+            // closing
+            if (ch == ')') {
+                int count = 0;
+                while (s.peek() != '(') {
+                    s.pop();
+                    count++;
+                }
+                if (count < 1) {
+                    return true; // duplicate mil gya
+                } else {
+                    s.pop(); // opening pair ko po krrdenege
+                }
+            } else { // opening
+                s.push(ch);
+            }
+
+        }
+        return false;
+    }
+
+    public static void main(String args[]) {
+        String str1 = "(a+b)";
+        String str2 = "((a+b))";
+
+        System.out.println(isDuplicate(str2));
+    }
+}
