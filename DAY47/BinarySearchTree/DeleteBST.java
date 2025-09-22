0b27664e7b627bd98903bcf4aa2375116377eca2
Divya Sharma <divyasharma11245@gmail.com>
2025-09-22 23:25:55 +0530

    DAY47--> Binary tree transform sum tree,, binary search tree basic concepts, insert in bst search in bst and delete in bst

---
+++ DAY47/BinarySearchTree/DeleteBST.java
@@ -0,0 +1,91 @@
+package BinarySearchTree;
+
+public class DeleteBST {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            root = new Node(val);
+            return root;
+        }
+
+        if (root.data > val) {
+            // left subtree
+            root.left = insert(root.left, val);
+        } else {
+            // right subtree
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+
+    public static void inOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+
+    }
+
+    public static Node delete(Node root, int key) {
+        if (root.data > key) {
+            root.left = delete(root.left, key);
+        } else if (root.data < key) {
+            root.right = delete(root.right, key);
+
+        } else { // voila
+            // CASES
+            // case1 --> no child
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+            // case 2 --> single child
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+
+            // case 3 --> two children
+            Node IS = findInorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+        }
+
+        return root;
+    }
+
+    public static Node findInorderSuccessor(Node root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        int value[] = { 8, 5, 3, 1, 4, 6, 10, 11, 14 };
+        Node root = null;
+
+        for (int i = 0; i < value.length; i++) {
+            root = insert(root, value[i]);
+        }
+
+        inOrder(root);
+        System.out.println();
+
+        delete(root, 8);
+        System.out.println();
+        inOrder(root);
+    }
+}
---
+++ DAY47/BinarySearchTree/SearchBST.java
@@ -0,0 +1,77 @@
+package BinarySearchTree;
+
+public class SearchBST {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            root = new Node(val);
+            return root;
+        }
+
+        if (root.data > val) {
+            // left subtree
+            root.left = insert(root.left, val);
+        } else {
+            // right subtree
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+
+    public static void inOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+
+    }
+
+    public static boolean search(Node root, int key) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.data == key) {
+            return true;
+        }
+
+        if (root.data > key) {
+            // left subtree
+            return search(root.left, key);
+        } else {
+            // right subtree
+            return search(root.right, key);
+        }
+    }
+
+    public static void main(String[] args) {
+        int value[] = { 5, 1, 3, 4, 2, 7 };
+        Node root = null;
+
+        for (int i = 0; i < value.length; i++) {
+            root = insert(root, value[i]);
+        }
+
+        inOrder(root);
+        System.out.println();
+
+        if (search(root, 8)) {
+            System.out.println("found");
+        } else {
+            System.out.println("not found!");
+        }
+    }
+}
