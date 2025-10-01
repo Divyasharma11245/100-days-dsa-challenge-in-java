d0fa02a554a16cea7b66817839ff5e0fbb574fe3
Divya Sharma <divyasharma11245@gmail.com>
2025-10-01 20:55:38 +0530

    DAY 51 --> priority queues and heap basic operations

---
+++ DAY51/PriorityQueueB.java
@@ -0,0 +1,38 @@
+
+// package DAY51;
+import java.util.*;
+
+public class PriorityQueueB {
+
+    static class Student implements Comparable<Student> { // comparable interface
+        String name;
+        int rank;
+
+        Student(String name, int rank) {
+            this.name = name;
+            this.rank = rank;
+        }
+
+        @Override // method overrding
+        public int compareTo(Student s2) {
+            return this.rank - s2.rank;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        PriorityQueue<Student> pq = new PriorityQueue<>(); // reverse order me krrne k liye -->
+                                                           // Comparator.reverseOrder()
+
+        pq.add(new Student("A", 4)); // O(logn)
+        pq.add(new Student("B", 5));
+        pq.add(new Student("C", 2));
+        pq.add(new Student("D", 12));
+
+        while (!pq.isEmpty()) {
+            System.out.println(pq.peek().name + " --> " + pq.peek().rank); // O(1)
+            pq.remove(); // O(logn)
+        }
+    }
+
+}
