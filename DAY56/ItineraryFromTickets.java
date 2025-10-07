91d3273b89fb3918b045ffae42f5c5b9b1ee4db1
Divya Sharma <divyasharma11245@gmail.com>
2025-10-07 17:46:24 +0530

    DAY56 --> hashmap and hashset questions,,, countdistinct elements, itinery from tickets, union and intersection of two arrays, longest subarray sum equal to 0

---
+++ DAY56/ItineraryFromTickets.java
@@ -0,0 +1,41 @@
+
+// package DAY56;
+import java.util.*;
+
+public class ItineraryFromTickets {
+
+    public static String getStart(HashMap<String, String> map) {
+        HashMap<String, String> revMap = new HashMap<>();
+
+        for (String key : map.keySet()) {
+            revMap.put(map.get(key), key);
+        }
+
+        // comparison
+        for (String key : map.keySet()) {
+            if (!revMap.containsKey(key)) {
+                return key; // will give my starting point
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+
+        map.put("Chennai", "Bengaluru");
+        map.put("Mumbai", "Delhi");
+        map.put("Goa", "Chennai");
+        map.put("Delhi", "Goa");
+
+        String start = getStart(map);
+        System.out.print(start);
+
+        for (String key : map.keySet()) {
+            System.out.print("-->" + map.get(start));
+            start = map.get(start);
+        }
+        System.out.println();
+
+    }
+}
