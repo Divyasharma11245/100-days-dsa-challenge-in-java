b83c22d1213e547be5113d06613756940d3ea8df
Divya Sharma <divyasharma11245@gmail.com>
2025-10-12 22:55:51 +0530

    Day61 --> components traversal--> modified bfs and dfs ,, cycle detetcion,, bipartite detetction

---
+++ DAY61/componentsTraversal/DfsModified.java
@@ -0,0 +1,88 @@
+package DAY61.componentsTraversal;
+
+import java.util.ArrayList;
+
+public class DfsModified {
+
+    // adgecency list
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int src, int dest, int wt) {
+            this.src = src;
+            this.dest = dest;
+            this.wt = wt;
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge> graph[]) {
+
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        // index - 0
+        graph[0].add(new Edge(0, 1, 1));
+        graph[0].add(new Edge(0, 2, 1));
+
+        // index - 1
+        graph[1].add(new Edge(1, 0, 1));
+        graph[1].add(new Edge(1, 3, 1));
+
+        // index - 2
+        graph[2].add(new Edge(2, 0, 1));
+        graph[2].add(new Edge(2, 4, 1));
+
+        // index - 3
+        graph[3].add(new Edge(3, 1, 1));
+        graph[3].add(new Edge(3, 5, 1));
+        graph[3].add(new Edge(3, 4, 1));
+
+        // index - 4
+        graph[4].add(new Edge(4, 2, 1));
+        graph[4].add(new Edge(4, 3, 1));
+        graph[4].add(new Edge(4, 5, 1));
+
+        // index - 5
+        graph[5].add(new Edge(5, 3, 1));
+        graph[5].add(new Edge(5, 4, 1));
+        graph[5].add(new Edge(5, 6, 1));
+
+        // index - 6
+        graph[6].add(new Edge(6, 5, 1));
+
+    }
+
+    public static void dfs(ArrayList<Edge>[] graph) {
+        boolean vis[] = new boolean[graph.length];
+
+        for (int i = 0; i < graph.length; i++) {
+            if (!vis[i]) {
+                dfsUtil(graph, i, vis);
+            }
+        }
+    }
+
+    public static void dfsUtil(ArrayList<Edge>[] graph, int curr, boolean vis[]) { // O(V+E)
+        System.out.println(curr + " ");
+        vis[curr] = true;
+
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            if (!vis[e.dest]) {
+                dfsUtil(graph, e.dest, vis);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int v = 7;
+        ArrayList<Edge>[] graph = new ArrayList[v];
+        createGraph(graph);
+
+        dfs(graph, 0, new boolean[v]);
+    }
+}
