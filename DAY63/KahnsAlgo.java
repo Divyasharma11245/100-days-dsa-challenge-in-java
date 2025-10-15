6c96c1cd0e980376a0884fcaeb797e887a611ed6
Divya Sharma <divyasharma11245@gmail.com>
2025-10-15 21:26:06 +0530

    DAy 63 --> kahns algorithm explration --topological sorting,,, all paths from source to destibation

---
+++ DAY63/KahnsAlgo.java
@@ -0,0 +1,84 @@
+package DAY63;
+
+import java.util.ArrayList;
+
+import DAY30.LinkedList;
+
+public class KahnsAlgo {
+    static class Edge {
+        int src;
+        int dest;
+
+        public Edge(int src, int dest) {
+            this.src = src;
+            this.dest = dest;
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge> graph[]) {
+
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        // index - 2
+        graph[2].add(new Edge(2, 3));
+
+        // index - 3
+        graph[3].add(new Edge(3, 1));
+
+        // index - 4
+        graph[4].add(new Edge(4, 0));
+        graph[4].add(new Edge(4, 1));
+
+        graph[5].add(new Edge(5, 0));
+        graph[5].add(new Edge(5, 2));
+
+    }
+
+    // topological sorting using kahn's algorithm
+    // topological sorting using bfs
+
+    public static void calcIndeg(ArrayList<Edge> graph[], int indeg[]) {
+        for (int i = 0; i < graph.length; i++) {
+            int v = i;
+            for (int j = 0; j < graph[v].size(); j++) {
+                Edge e = graph[v].get(j);
+                indeg[e.dest]++;
+            }
+        }
+    }
+
+    public static void topSort(ArrayList<Edge> graph[]) {
+        int indeg[] = new int[graph.length];
+        Queue<Integer> q = new LinkedList();
+
+        for (int i = 0; i < indeg.length; i++) {
+            if (indeg[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+            System.out.println(curr + " ");
+
+            for (int i = 0; i < graph[curr].size(); i++) {
+                Edge e = graph[curr].get(i);
+                indeg[e.dest]--;
+                if (indeg[e.dest] == 0) {
+                    q.add(e.dest);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int v = 6;
+        ArrayList<Edge>[] graph = new ArrayList[v];
+        createGraph(graph);
+
+        topSort(graph);
+    }
+}
