a78041319b06b7e00d827e5b9abbc79c0ac70f73
Divya Sharma <divyasharma11245@gmail.com>
2025-10-27 23:00:34 +0530

    day 73 -> basics of dp

---
+++ DAY73/DP/Fibonacci.java
@@ -0,0 +1,42 @@
+public class Fibonacci {
+
+    // RECURSICE FUNCTION
+    public static int fib(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    // STORE THE REPEATED RECURSIVE CALLS -->MEMOIZATION
+    public static int fib(int n, int f[]) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        if (f[n] != 0) {
+            return f[n];
+        }
+        f[n] = fib(n - 1) + fib(n - 2);
+        return f[n];
+    }
+
+    // TABULATED FORM --> TABULATION
+    public static int fibTab(int n) {
+        int dp[] = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n]; // ans
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int f[] = new int[n + 1];
+        System.out.println(fib(n, f));
+        System.out.println(fibTab(n));
+
+    }
+
+}
