4b1b8ad7edc826a99f4362a0f865773a4edafd51
Divya Sharma <divyasharma11245@gmail.com>
2025-10-31 00:16:03 +0530

    Day 76 --> dp -> target sum, unbounded knapsack and coin change

---
+++ DAY76/CoinChange.java
@@ -0,0 +1,33 @@
+package DAY76;
+
+public class CoinChange {
+
+    public static int waysTab(int coins[], int sum) {
+        int n = coins.length;
+        int dp[][] = new int[n + 1][sum + 1];
+
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j = 0; j < dp[0].length; j++) {
+            dp[0][j] = 0;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < sum + 1; j++) {
+                if (coins[i - 1] <= j) { // valid
+                    dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j]; // total number of ways
+                } else { // invalid
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+
+    public static void main(String[] args) {
+        int coins[] = { 2, 5, 3, 6 };
+        int sum = 10;
+        System.out.println(waysTab(coins, sum));
+    }
+}
