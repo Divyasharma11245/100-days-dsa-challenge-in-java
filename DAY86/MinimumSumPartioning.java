43c799789264f4903d0ae3652cb40db90567c391
Divya Sharma <divyasharma11245@gmail.com>
2025-11-10 20:39:12 +0530

    DAY86 ---> minimum sum partition ans minimum jumps on an array

---
+++ DAY86/MinimumSumPartioning.java
@@ -0,0 +1,33 @@
+public class MinimumSumPartioning {
+
+    public static int minSum(int arr[]) {
+        int n = arr.length;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+
+        int W = sum / 2;
+        int dp[][] = new int[n + 1][W + 1];
+
+        // bottom up
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < W + 1; j++) {
+                if (arr[i - 1] <= j) {
+                    dp[i][j] = Math.max(arr[i - 1] + dp[i - 1][j - arr[i - 1]], dp[i - 1][j]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        int sum1 = dp[n][W];
+        int sum2 = sum - sum1;
+        int ans = Math.abs(sum1 - sum2);
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 6, 11, 5 };
+        System.out.println(minSum(arr));
+    }
+}
